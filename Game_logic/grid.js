2fd24536539ae65b1f9d7c7e55bb59b3366c8f13
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-09-29 12:26:56 -0400

    Look at the previous commit message

--- Game_logic/grid.js
+++ Game_logic/grid.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createCountGrid = exports.createBombGrid = void 0;
 //These enums are being used to store the default game size and Number of bombs
 //Based on the Difficulty of Easy, Medium, and Hard
 var GameDifficulty;
@@ -13,70 +15,113 @@ var NumOfBombs;
     NumOfBombs[NumOfBombs["Medium"] = 40] = "Medium";
     NumOfBombs[NumOfBombs["Hard"] = 100] = "Hard";
 })(NumOfBombs || (NumOfBombs = {}));
-//Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
-let row = GameDifficulty.Easy / 10;
-let col = GameDifficulty.Easy / 10;
-let countGrid = new Array(row);
-let bombGrid = new Array(row);
-for (let i = 0; i < row; i++) {
-    bombGrid[i] = new Array(col);
-    countGrid[i] = new Array(col);
-}
-//Bomb Grid creation (Need to make this as an exported function)
-let bombCount = NumOfBombs.Easy;
-//This loop is to insert all of the bombs;
-for (let i = 0; i < bombCount; i++) {
-    let bombRow = Math.floor(Math.random() * row);
-    let bombCol = Math.floor(Math.random() * col);
-    if (bombGrid[bombRow][bombCol] === true) {
-        i--;
+function createBombGrid(gameDifficulty) {
+    //Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
+    let row = 0;
+    let col = 0;
+    let bombCount = 0;
+    if (gameDifficulty.toLowerCase() === "easy") {
+        row = GameDifficulty.Easy / 10;
+        col = GameDifficulty.Easy / 10;
+        bombCount = NumOfBombs.Easy;
     }
-    else {
-        bombGrid[bombRow][bombCol] = true;
+    if (gameDifficulty.toLowerCase() === "medium") {
+        row = GameDifficulty.Medium / 14;
+        col = GameDifficulty.Medium / 18;
+        bombCount = NumOfBombs.Medium;
     }
-}
-let count = 0;
-for (let i = 0; i < row * col; i++) {
-    let currRow = Math.floor((i) / row);
-    let currCol = (i) % col;
-    bombGrid[currRow][currCol] != true ? bombGrid[currRow][currCol] = false : count++;
-    console.log(`Element ${currRow},${currCol} is ${bombGrid[currRow][currCol]}`);
-}
-console.log(`Number of bombs are ${count}`);
-// let currRow = Math.floor((i) / row);
-// let currCol = (i) % col;
-//Created the count grid here
-for (let i = 0; i < row * col; i++) {
-    let currRow = Math.floor((i) / row);
-    let currCol = (i) % col;
+    if (gameDifficulty.toLowerCase() === "hard") {
+        row = GameDifficulty.Hard / 20;
+        col = GameDifficulty.Hard / 24;
+        bombCount = NumOfBombs.Hard;
+    }
+    let bombGrid = new Array(row);
+    for (let i = 0; i < row; i++) {
+        bombGrid[i] = new Array(col);
+    }
+    //Bomb Grid creation (Need to make this as an exported function)
+    //This loop is to insert all of the bombs;
+    for (let i = 0; i < bombCount; i++) {
+        let bombRow = Math.floor(Math.random() * row);
+        let bombCol = Math.floor(Math.random() * col);
+        if (bombGrid[bombRow][bombCol] === true) {
+            i--;
+        }
+        else {
+            bombGrid[bombRow][bombCol] = true;
+        }
+    }
     let count = 0;
-    if (bombGrid[currRow][currCol] == true) {
-        continue;
+    for (let i = 0; i < row * col; i++) {
+        let currRow = Math.floor((i) / row);
+        let currCol = (i) % col;
+        bombGrid[currRow][currCol] != true ? bombGrid[currRow][currCol] = false : count++;
+        console.log(`Element ${currRow},${currCol} is ${bombGrid[currRow][currCol]}`);
     }
-    if (currRow != 0 && bombGrid[currRow - 1][currCol] == true) {
-        count++;
-    }
-    if (currRow != row - 1 && bombGrid[currRow + 1][currCol] == true) {
-        count++;
-    }
-    if (currCol != 0 && bombGrid[currRow][currCol - 1] == true) {
-        count++;
-    }
-    if (currCol != col - 1 && bombGrid[currRow][currCol + 1] == true) {
-        count++;
-    }
-    if ((currRow != 0 && currCol != 0) && bombGrid[currRow - 1][currCol - 1] == true) {
-        count++;
-    }
-    if ((currRow != row - 1 && currCol != 0) && bombGrid[currRow + 1][currCol - 1] == true) {
-        count++;
-    }
-    if ((currRow != 0 && currCol != col - 1) && bombGrid[currRow - 1][currCol + 1]) {
-        count++;
-    }
-    if ((currRow != row - 1 && currCol != col - 1) && bombGrid[currRow + 1][currCol + 1]) {
-        count++;
-    }
-    countGrid[currRow][currCol] = count;
-    console.log(`This is the count for element ${currRow},${currCol}: ${count}`);
+    return bombGrid;
 }
+exports.createBombGrid = createBombGrid;
+createBombGrid("Easy");
+function createCountGrid(gameDifficulty, bombGrid) {
+    //Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
+    let row = 0;
+    let col = 0;
+    if (gameDifficulty.toLowerCase() === "easy") {
+        row = GameDifficulty.Easy / 10;
+        col = GameDifficulty.Easy / 10;
+    }
+    if (gameDifficulty.toLowerCase() === "medium") {
+        row = GameDifficulty.Medium / 14;
+        col = GameDifficulty.Medium / 18;
+    }
+    if (gameDifficulty.toLowerCase() === "hard") {
+        row = GameDifficulty.Hard / 20;
+        col = GameDifficulty.Hard / 24;
+    }
+    let countGrid = new Array(row);
+    for (let i = 0; i < row; i++) {
+        countGrid[i] = new Array(col);
+    }
+    // let currRow = Math.floor((i) / row);
+    // let currCol = (i) % col;
+    //Created the count grid here
+    for (let i = 0; i < row * col; i++) {
+        let currRow = Math.floor((i) / row);
+        let currCol = (i) % col;
+        let count = 0;
+        if (bombGrid[currRow][currCol] == true) {
+            continue;
+        }
+        if (currRow != 0 && bombGrid[currRow - 1][currCol] == true) {
+            count++;
+        }
+        if (currRow != row - 1 && bombGrid[currRow + 1][currCol] == true) {
+            count++;
+        }
+        if (currCol != 0 && bombGrid[currRow][currCol - 1] == true) {
+            count++;
+        }
+        if (currCol != col - 1 && bombGrid[currRow][currCol + 1] == true) {
+            count++;
+        }
+        if ((currRow != 0 && currCol != 0) && bombGrid[currRow - 1][currCol - 1] == true) {
+            count++;
+        }
+        if ((currRow != row - 1 && currCol != 0) && bombGrid[currRow + 1][currCol - 1] == true) {
+            count++;
+        }
+        if ((currRow != 0 && currCol != col - 1) && bombGrid[currRow - 1][currCol + 1]) {
+            count++;
+        }
+        if ((currRow != row - 1 && currCol != col - 1) && bombGrid[currRow + 1][currCol + 1]) {
+            count++;
+        }
+        countGrid[currRow][currCol] = count;
+        console.log(`This is the count for element ${currRow},${currCol}: ${count}`);
+    }
+    return countGrid;
+}
+exports.createCountGrid = createCountGrid;
+let bomb = createBombGrid("easy");
+let count = createCountGrid("easy", bomb);
+console.log(bomb.length + bomb[0].length + " " + count.length);
--- Game_logic/grid.ts
+++ Game_logic/grid.ts
@@ -12,78 +12,116 @@ enum NumOfBombs {
     Hard = 100
 }
 
-//Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
-let row: number = GameDifficulty.Easy / 10;
-let col: number = GameDifficulty.Easy / 10;
-let countGrid: number[][] = new Array(row);
-let bombGrid: boolean[][] = new Array(row);
-
-for(let i = 0; i < row; i++){
-    bombGrid[i] = new Array(col);
-    countGrid[i] = new Array(col);
-}
-
-//Bomb Grid creation (Need to make this as an exported function)
-let bombCount: number = NumOfBombs.Easy;
-//This loop is to insert all of the bombs;
-for(let i = 0; i < bombCount; i++){
-    let bombRow = Math.floor(Math.random() * row);
-    let bombCol = Math.floor(Math.random() * col);
-
-    if(bombGrid[bombRow][bombCol] === true){
-        i--;
-    } 
-    else{
-        bombGrid[bombRow][bombCol] = true;
-    }
-}
-
-let count: number = 0;
-for(let i = 0; i < row * col; i++){
-    let currRow = Math.floor((i) / row);
-    let currCol = (i) % col;
-
-    bombGrid[currRow][currCol] != true ? bombGrid[currRow][currCol] = false : count++;
-    console.log(`Element ${currRow},${currCol} is ${bombGrid[currRow][currCol]}`);
-}
-console.log(`Number of bombs are ${count}`);
 // let currRow = Math.floor((i) / row);
 // let currCol = (i) % col;
 
-//Created the count grid here
+export function createBombGrid(gameDifficulty: string): boolean[][]{
+    //Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
+    let row: number = 0;
+    let col: number = 0;
+    let bombCount: number = 0;
+    if(gameDifficulty.toLowerCase() === "easy"){
+        row = GameDifficulty.Easy / 10;
+        col = GameDifficulty.Easy / 10;
+        bombCount= NumOfBombs.Easy;
+    }
+    if(gameDifficulty.toLowerCase() === "medium"){
+        row = GameDifficulty.Medium / 14;
+        col = GameDifficulty.Medium / 18;
+        bombCount= NumOfBombs.Medium;
+    }
+    if(gameDifficulty.toLowerCase() === "hard"){
+        row = GameDifficulty.Hard / 20;
+        col = GameDifficulty.Hard / 24;
+        bombCount= NumOfBombs.Hard;
+    }
+    
+    let bombGrid: boolean[][] = new Array(row);
 
-for(let i = 0; i < row * col; i++){
-    let currRow = Math.floor((i) / row);
-    let currCol = (i) % col;
-    let count: number = 0;
+    for(let i = 0; i < row; i++){
+        bombGrid[i] = new Array(col);
+    }
 
-    if(bombGrid[currRow][currCol] == true){
-        continue;
+    //Bomb Grid creation (Need to make this as an exported function)
+    
+    //This loop is to insert all of the bombs;
+    for(let i = 0; i < bombCount; i++){
+        let bombRow = Math.floor(Math.random() * row);
+        let bombCol = Math.floor(Math.random() * col);
+
+        bombGrid[bombRow][bombCol] === true ? i-- : bombGrid[bombRow][bombCol] = true;
     }
-    if(currRow != 0 && bombGrid[currRow-1][currCol] == true){
-        count++;
+
+    for(let i = 0; i < row * col; i++){
+        let currRow = Math.floor((i) / row);
+        let currCol = (i) % col;
+
+        if(bombGrid[currRow][currCol] != true){
+            bombGrid[currRow][currCol] = false
+        }
     }
-    if(currRow != row-1 && bombGrid[currRow+1][currCol] == true){
-        count++;
+    return bombGrid;
+}
+
+createBombGrid("Easy");
+
+export function createCountGrid(gameDifficulty: string, bombGrid: boolean[][]): number[][]{
+    //Creating and initializing the 2d array with a for loop since typescript doesn't do that by default
+    let row: number = 0;
+    let col: number = 0;
+    if(gameDifficulty.toLowerCase() === "easy"){
+        row = GameDifficulty.Easy / 10;
+        col = GameDifficulty.Easy / 10;
     }
-    if(currCol != 0 && bombGrid[currRow][currCol-1] == true){
-        count++;
+    if(gameDifficulty.toLowerCase() === "medium"){
+        row = GameDifficulty.Medium / 14;
+        col = GameDifficulty.Medium / 18;
     }
-    if(currCol != col-1 && bombGrid[currRow][currCol+1] == true){
-        count++;
+    if(gameDifficulty.toLowerCase() === "hard"){
+        row = GameDifficulty.Hard / 20;
+        col = GameDifficulty.Hard / 24;
     }
-    if((currRow != 0 && currCol != 0) && bombGrid[currRow-1][currCol-1] == true){
-        count++;
+    let countGrid: number[][] = new Array(row);
+
+    for(let i = 0; i < row; i++){
+        countGrid[i] = new Array(col);
     }
-    if((currRow != row-1 && currCol != 0) && bombGrid[currRow+1][currCol-1] == true){
-        count++;
+
+
+    //Created the count grid here
+    for(let i = 0; i < row * col; i++){
+        let currRow = Math.floor((i) / row);
+        let currCol = (i) % col;
+        let count: number = 0;
+
+        if(bombGrid[currRow][currCol] == true){
+            continue;
+        }
+        if(currRow != 0 && bombGrid[currRow-1][currCol] == true){
+            count++;
+        }
+        if(currRow != row-1 && bombGrid[currRow+1][currCol] == true){
+            count++;
+        }
+        if(currCol != 0 && bombGrid[currRow][currCol-1] == true){
+            count++;
+        }
+        if(currCol != col-1 && bombGrid[currRow][currCol+1] == true){
+            count++;
+        }
+        if((currRow != 0 && currCol != 0) && bombGrid[currRow-1][currCol-1] == true){
+            count++;
+        }
+        if((currRow != row-1 && currCol != 0) && bombGrid[currRow+1][currCol-1] == true){
+            count++;
+        }
+        if((currRow != 0 && currCol != col-1) && bombGrid[currRow-1][currCol+1]){
+            count++;
+        }
+        if((currRow != row-1 && currCol != col-1) && bombGrid[currRow+1][currCol+1]){
+            count++;
+        }
+        countGrid[currRow][currCol] = count; 
     }
-    if((currRow != 0 && currCol != col-1) && bombGrid[currRow-1][currCol+1]){
-        count++;
-    }
-    if((currRow != row-1 && currCol != col-1) && bombGrid[currRow+1][currCol+1]){
-        count++;
-    }
-    countGrid[currRow][currCol] = count; 
-    console.log(`This is the count for element ${currRow},${currCol}: ${count}`);
+    return countGrid;
 }
